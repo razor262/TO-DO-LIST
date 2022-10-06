97c369d16514667be1a402d6a84d77ed08a41b1b
raoz262 <24.work@bk.ru>
2022-10-06 04:06:47 +0300

    this: my new commit

--- src/index.js
+++ src/index.js
@@ -6,42 +6,44 @@ let btn = document.querySelector('.btn')
 let div = document.createElement("div");
 div.className = "container"
 div.id = "content";
-div.className = "note";
 
 let input = document.createElement("input");
 input.className = "input"
 div.appendChild(input);
-/*
-let btn = document.createElement("button");
-btn.className = "newButton"
-btn.textContent = "create";
-div.appendChild(btn);
-*/
+
 document.body.appendChild(div);
 
 btn.addEventListener("click", myFunction);
 
 function myFunction() {
+if(input.value == 0){
+    alert('Добавьте задачу')
+} else {
 
-  let btn_1 = document.createElement("button");
-  btn_1.className = "delButton"
-  btn_1.textContent = "delited";
-  div.appendChild(btn_1);
+    let btn_1 = document.createElement("button");
+    btn_1.className = "delButton";
+    btn_1.textContent = "delited";
+    div.appendChild(btn_1);
+  
+    // create a new heading and add it to the div
+    let p = document.createElement("p");
+    p.textContent = input.value;
+    div.appendChild(p);
+    input.value = ''
+  
+    // add div to the document
+    document.body.appendChild(div);
+  
+    btn_1.addEventListener('click', deliteFunction)
 
-  // create a new heading and add it to the div
-  let p = document.createElement("p");
-  p.textContent = input.value;
-  div.appendChild(p);
-
-  // add div to the document
-  document.body.appendChild(div);
-
-  btn_1.addEventListener('click', deliteFunction)
-
-  function deliteFunction() {
-    //alert('delite')
-    div.removeChild(p)
-    div.removeChild(btn)
+    function deliteFunction() {
+        //alert('delite')
+        div.removeChild(p)
+        div.removeChild(btn_1)
+    }
+  
 }
+
+
 }
 
